cecfda23fb5cc9eccd3e7caa11a38c89fa2e89b2
Gianluca Costa <gianluca@gianlucacosta.info>
2022-08-18 14:09:45 +0200

    Use process.exitCode in lieu of a raw exit()

--- src/bin/copy.ts
+++ src/bin/copy.ts
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 
 import { join } from "node:path";
-import { argv } from "node:process";
+import process, { argv } from "node:process";
 import { copySync } from "fs-extra";
 
-const targetDirectoryPath = argv[2];
+function main(): number {
+  const targetDirectoryPath = argv[2];
 
-if (!targetDirectoryPath) {
-  console.error("Missing CLI argument: <target directory>");
-  process.exit(1);
+  if (!targetDirectoryPath) {
+    console.error("Missing CLI argument: <target directory>");
+    return 1;
+  }
+
+  const sourceDirectoryPath = join(__dirname, "..", "web");
+
+  console.log("Copying frontend files");
+  console.log(`    from --> ${sourceDirectoryPath}`);
+  console.log(`    to --> ${targetDirectoryPath}`);
+
+  copySync(sourceDirectoryPath, targetDirectoryPath);
+  return 0;
 }
 
-const sourceDirectoryPath = join(__dirname, "..", "web");
-
-console.log("Copying frontend files");
-console.log(`    from --> ${sourceDirectoryPath}`);
-console.log(`    to --> ${targetDirectoryPath}`);
-
-copySync(sourceDirectoryPath, targetDirectoryPath);
+process.exitCode = main();
